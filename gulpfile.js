de6906dc65f46986c50c18e7254124841f3acf9f
Preston Pham <prestonp08@gmail.com>
2015-05-15 16:35:26 -0500

    init demo page
    add live reloading
    
    add js watching
    
    readme
    
    add syntax highlighter
    
    align right example
    
    header
    
    cleanup
    
    0.0.1
    
    rm npm-debug

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var minifyCSS     = require('gulp-minify-css');
 var uglify        = require('gulp-uglify');
 var ver           = require('gulp-ver');
 var path          = require('path');
+var browserSync   = require('browser-sync');
+var reload        = browserSync.reload;
 
 gulp.task('less', function() {
   return gulp.src('src/**/*.less')
@@ -12,14 +14,27 @@ gulp.task('less', function() {
     }))
     .pipe(minifyCSS())
     .pipe(ver())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(reload({ stream: true }));
 });
 
 gulp.task('compress', function() {
   return gulp.src('src/**/*.js')
     .pipe(uglify())
     .pipe(ver())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(reload({ stream: true }));
+});
+
+gulp.task('serve', ['less', 'compress'], function() {
+  browserSync({
+    server: {
+      baseDir: '.'
+    }
+  });
+
+  gulp.watch('src/**/*.less', ['less']);
+  gulp.watch('src/**/*.js', ['compress']);
 });
 
 gulp.task('default', [ 'less', 'compress' ]);
